37c1893220a87341602786d10e3f5ad094d93a4a
Ryan-Liebregts <ryanliebregts17@gmail.com>
2023-09-20 23:39:43 +0800

    oqjwebdkjhwqebf

--- app/src/main/java/com/example/madassignment/BoardFragment.java
+++ app/src/main/java/com/example/madassignment/BoardFragment.java
@@ -287,7 +287,7 @@ public class BoardFragment extends Fragment implements BoardButtonAdapter.Adapte
         hasTimerRanOut = false;
 
         // Start timer
-        //startTimer();
+        startTimer();
         if(!currentTimerExists) startTimer();
 
         /* -----------------------------------------------------------------------------------------
@@ -886,7 +886,7 @@ public class BoardFragment extends Fragment implements BoardButtonAdapter.Adapte
 
         // Reset timer
         stopTimer();
-        //startTimer();
+        startTimer();
     }
 
     /* -----------------------------------------------------------------------------------------
@@ -1025,7 +1025,7 @@ public class BoardFragment extends Fragment implements BoardButtonAdapter.Adapte
         else{
             // Reset timer
             stopTimer();
-            //startTimer();
+            startTimer();
         }
 
         // Update game board and move list in gameData
@@ -1043,7 +1043,7 @@ public class BoardFragment extends Fragment implements BoardButtonAdapter.Adapte
                 } else {
                     // Reset timer
                     stopTimer();
-                    //startTimer();
+                    startTimer();
                 }
             }
         }, delayMillis);
@@ -1222,6 +1222,8 @@ public class BoardFragment extends Fragment implements BoardButtonAdapter.Adapte
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+        stopTimer();
+        startTimer();
 
         // Save game-related data to the bundle
         outState.putBoolean("needSaveState", gameData.getNeedSaveGameState());
